644f92e3eeceb5d4148985bef810b00a6747196f
Manuel Tarby <manuel.tarby@gmail.com>
2026-07-12 09:12:52 +0200

    Moved Matches page into Matches repository

--- Services/MatchService.cs
+++ Services/MatchService.cs
@@ -433,7 +433,9 @@ namespace TableTennisHistoric.Services
                     {
                         Value = cc.Id.ToString(),
                         Text = cc.Competition.Name
-                    }).ToListAsync();
+                    })
+                    .OrderBy(cc => cc.Text)
+                    .ToListAsync();
             }
 
             var opponents = (await _playerService.GetAllPlayersAsync() ?? new List<Player>())
@@ -544,7 +546,7 @@ namespace TableTennisHistoric.Services
                     Id = m.Id,
                     Date_of_match = m.Date_match,
                     Competition = m.CompetitionCoefficient.Competition.Name,
-                    CompetitionSupplementName = m.CompetitionSupplement.Name,
+                    CompetitionSupplementName = m.CompetitionSupplement == null ? "" : m.CompetitionSupplement.Name,
                     Coefficient = m.CompetitionCoefficient.Coefficient,
                     OpponnentId = m.OpponentId,
                     Opponent_first_name = m.Opponent.First_name,
